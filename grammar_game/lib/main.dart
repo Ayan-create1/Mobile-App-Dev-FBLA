719ba3cf99dec8822e694f023f40cda1a3c6f301
Farooq Ahmed <farooq.ahmed.0107@gmail.com>
2025-01-15 18:06:06 -0700

    Adding some come (Does not work yet)

--- grammar_game/lib/main.dart
+++ grammar_game/lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
 }
 
- class MyApp extends StatelessWidget {
-   const MyApp({super.key});
- 
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp();
-   }
- }
--- grammar_game/lib/project/matching.dart
+++ grammar_game/lib/project/matching.dart
@@ -1,2 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:petitparser/petitparser.dart';
+
+class MatchingGame extends StatefulWidget {
+  @override
+  _MatchingGameState createState() => _MatchingGameState();
+}
+
+class _MatchingGameState extends State<MatchingGame> {
+  List<String> words = ['apple', 'banana', 'cherry'];
+  String selectedWord = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        DragTarget<String>(
+          onAccept: (data) {
+            setState(() {
+              selectedWord = data;
+            });
+          },
+          builder: (context, candidateData, rejectedData) {
+            return Container(
+              width: 200,
+              height: 50,
+              color: Colors.grey[200],
+              child: Center(child: Text(selectedWord.isEmpty ? 'Drop here' : selectedWord)),
+            );
+          },
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: words.map((word) {
+            return Draggable<String>(
+              data: word,
+              child: Container(
+                width: 100,
+                height: 50,
+                color: Colors.blue,
+                child: Center(child: Text(word)),
+              ),
+              feedback: Container(
+                width: 100,
+                height: 50,
+                color: Colors.blue.withOpacity(0.5),
+                child: Center(child: Text(word)),
+              ),
+              childWhenDragging: Container(
+                width: 100,
+                height: 50,
+                color: Colors.grey,
+              ),
+            );
+          }).toList(),
+        ),
+      ],
+    );
+  }
+}
+
+
