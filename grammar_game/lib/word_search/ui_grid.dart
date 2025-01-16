6a7d1b288dd3ce3511459d9e8844c667484c9537
Ayan-create1 <2ayanagarwal09@gmail.com>
2025-01-16 09:08:27 -0700

    Added comments to ui_grid.dart

--- grammar_game/lib/word_search/ui_grid.dart
+++ grammar_game/lib/word_search/ui_grid.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'word_grid.dart';
 
 //!Made using generative AI tools
+//*This class will display the wordsearch page
 class WordSearchGame extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
+    //*Will display the state of the grid
     return _WordGridState();
   }
 }
@@ -13,8 +15,9 @@ class _WordGridState extends State<WordSearchGame> {
   @override
   Widget build(BuildContext context) {
     //*Scaffold will define new page in application
+    //*Returns the entire project page
     return Scaffold(
-      //*Will show text login at the top
+      //*Return the appbar with title center, title with special text, and background properties
       appBar: AppBar(
         //*Will show title of screen and center that title
         centerTitle: true,
@@ -33,11 +36,18 @@ class _WordGridState extends State<WordSearchGame> {
 }
 
 Widget _buildUI() {
+  //! Maybe put this into one seperate module called display_grid
+  //*Here we will get a 2D grid
   List<List<String>> grid = test();
+  //*Here we create 1D array to append each row of containers to
   List<Widget> rows = [];
+  //*Making a traversal to traverse through each row in the grid
   for (var row in grid) {
+    //*Cells will hold each individual letter in each row of the 2D array grid
     List<Widget> cells = [];
+    //*for each letter in the row
     for (var cell in row) {
+      //Here we will make a container for each cell in the row
       cells.add(
         Container(
           alignment: Alignment.center,
@@ -59,6 +69,7 @@ Widget _buildUI() {
         ),
       );
     }
+    //*We will add the cells list into a row widget. Basically creating each row individually
     rows.add(
       Row(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -68,6 +79,7 @@ Widget _buildUI() {
   }
   return Scaffold(
     backgroundColor: Colors.black,
+    //*Will return a giant container full of a colum the will display each row of the 2D array
     body: Container(
       padding: const EdgeInsets.all(16.0),
       child: Column(
